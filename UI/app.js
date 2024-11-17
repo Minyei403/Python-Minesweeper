aabe63516850993ec8827dc1d3fb3b1b34fda140
Minyei403 <m222kim@uwaterloo.ca>
2024-11-17 16:49:48 -0500

    Update app.js

--- UI/app.js
+++ UI/app.js
@@ -3,7 +3,41 @@ var bombs = 0;
 var flaggedCells = [];
 var gameOver = false;
 var won = false;
+var timer; 
+var timeLeft = 60; // seconds
 
+// What to do when the timer runs out
+function gameOver() {
+  // This cancels the setInterval, so the updateTimer stops getting called
+  cancelInterval(timer);
+  
+  // re-show the button, so they can start it again
+  $('#playAgainButton').show();
+}
+
+function updateTimer() {
+  timeLeft = timeLeft - 1;
+  if(timeLeft >= 0)
+    $('#timer').html(timeLeft);
+  else {
+    gameOver();
+  }
+}
+
+// The button has an on-click event handler that calls this
+function start() {
+  // setInterval is a built-in function that will call the given function
+  // every N milliseconds (1 second = 1000 ms)
+  timer = setInterval(updateTimer, 1000);
+  
+  // It will be a whole second before the time changes, so we'll call the update
+  // once ourselves
+  updateTimer();
+  
+  // We don't want the to be able to restart the timer while it is running,
+  // so hide the button.
+   $('#playAgainButton').hide();
+}
 function startNew(mode) {
   // Easy   -> 7X10=70 	   -> 10 bombs  | 10 -> 15 bombs
   // medium -> 12X25=300   -> 40 bombs  | 18 -> 45 bombs
